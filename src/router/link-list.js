b85eb845bd4a2c9e558e59d9e082c12ed2e31828
Jandronit <jandrohen@gmail.com>
2023-05-28 12:26:50 +0200

    (lesson 310 course vue) Create link of definition in separate file

---
+++ src/router/link-list.js
@@ -0,0 +1,20 @@
+export const linksList = [
+  {
+    title: "Typography",
+    caption: "Types of Letters in Quasar",
+    icon: "las la-align-left",
+    link: "typography",
+  },
+  {
+    title: "Flex Layout",
+    caption: "Styling with Flexbox",
+    icon: "las la-layer-group",
+    link: "flex",
+  },
+  {
+    title: "Docs",
+    caption: "quasar.dev",
+    icon: "las la-graduation-cap",
+    link: "https://quasar.dev",
+  },
+];
