f54261a13ff3ad9460f190704f7beb3e665a47ac
Jandronit <jandrohen@gmail.com>
2023-05-28 21:47:52 +0200

    (lesson 314 course vue) Flex and Grid system

--- src/pages/Flex.vue
+++ src/pages/Flex.vue
@@ -1,6 +1,29 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1>flex</h1>
+  <q-page>
+    <div class="row q-mb-md">
+      <div class="col bg-primary">1</div>
+      <div class="col bg-secondary">2</div>
+      <div class="col bg-accent">3</div>
+    </div>
+
+    <div class="row q-mb-md">
+      <!-- 12 slots -->
+      <div class="col bg-primary">1</div>
+      <div class="col col-5 bg-secondary">2</div>
+      <div class="col col-6 bg-accent">3</div>
+    </div>
+
+    <div class="row q-mb-md justify-center">
+      <!-- 12 slots -->
+      <div class="col col-4 bg-secondary">2</div>
+      <div class="col col-4 bg-accent">3</div>
+    </div>
+
+    <div class="row q-mb-md justify-between">
+      <!-- 12 slots -->
+      <div class="col col-4 bg-secondary">2</div>
+      <div class="col col-4 bg-accent">3</div>
+    </div>
   </q-page>
 </template>
 
